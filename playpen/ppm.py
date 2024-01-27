9238a6cc4b011e7443459ff00b831818a2e1cedb
Xerxes Battiwalla <xerxesb@gmail.com>
2024-01-27 16:57:51 +1100

    Added progress to stderr

--- playpen/ppm.py
+++ playpen/ppm.py
@@ -1,6 +1,5 @@
 import os
-
-# render
+import sys
 
 width = 256
 height = 256
@@ -8,9 +7,13 @@ height = 256
 print(f"P3\n{width} {height}\n255\n")
 
 for j in range(height):
+  print(f"\rScanlines remaining: {height - j}", file=sys.stderr, end="")
+
   for i in range(width):
     r = i / (width - 1)
     g = j / (height - 1)
     b = 0
 
     print(f"{int(255.999 * r)} {int(255.999 * g)} {int(255.999 * b)}")
+
+print(f"\rDone", file=sys.stderr, end="")
